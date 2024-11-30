cb6c58174d1c00db349f3838a04c09d07c44bcf9
Neptali-Ricardo <neprcm@gmail.com>
2024-12-01 00:39:28 +0100

    Añadir cambios en los archivos SCSS y js, y agregar .gitignore

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// Importa los estilos CSS
+import './scss/main.scss'; // Asegúrate de que la ruta sea correcta
+
+// Agregar alguna lógica o interacción en JavaScript si es necesario
+console.log("El archivo index.js ha sido cargado correctamente");
+
+// Aquí puedes agregar más código JavaScript si es necesario
